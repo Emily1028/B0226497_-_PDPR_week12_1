997c333caaa8c54f1e1c956ef4b7c4fd10cd4790
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-01 23:56:22 +0800

    加入專案檔案。

---
+++ BOOKS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0226497_段昱如_PDPR_week12_1
+{
+    class BOOKS
+    {
+        public string NO { set; get; }
+        public string TITLE { set; get; }
+        public string AUTHOR { set; get; }
+        static int COUNTB = 0;
+        static int TOTALP;
+        public int PRICE, TOTALB = COUNTB += 1;
+        public BOOKS(string no, string title, string author, int price)
+        {
+            NO = no;
+            TITLE = title;
+            AUTHOR = author;
+            PRICE = price;
+            TOTALP += price;
+        }
+        public int AVERAGE()
+        {
+            return TOTALP / TOTALB;
+        }
+        public string printME()
+        {
+            return $"書號:{NO}，書名:{TITLE}，作者:{AUTHOR}，書價:{PRICE}，書本數:{TOTALB}，總價:{TOTALP}，平均書價:{AVERAGE()}\n";
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace B0226497_段昱如_PDPR_week12_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEXIT_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        String str = "";
+        Random rnd = new Random();
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            List<BOOKS> listgBOOKS = new List<BOOKS>();
+            for (int i = 0; i < 10; i++)
+            {
+                BOOKS book = new BOOKS($"B0226497{i}", $"安徒生童話_{i + 1}", $"安徒生", rnd.Next(100, 500));
+                str += book.printME();
+            }
+            lblRESULT.Text = str;
+        }
+
+
+    }
+}
